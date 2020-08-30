59cdb9a695150a7b0dc5565a08739b37e8b84cb9
Alemasov Sergey Aleksandrovich <s.alemasov@tinkoff.ru>
2020-08-30 13:40:21 +0400

    some changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to test-csgo!');
+    expect(page.getTitleText()).toEqual('Welcome to test-onmoon!');
   });
 
   afterEach(async () => {
